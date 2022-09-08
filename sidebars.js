19dd67d6a1343adc68bc9578aae9525adddb1326
Pavlos Tzegiannakis <pavlostze@gmail.com>
2022-09-08 12:58:21 +0300

    Remove Helium's references

--- sidebars.js
+++ sidebars.js
@@ -28,20 +28,7 @@ module.exports = {
             "wxm-devices/wifi-m5-bundle/m5-troubleshooting",
           ],
           collapsed: false,
-        },
-        {
-					type: "category",
-					label: "Helium Weather Station",
-					items: [
-  						"wxm-devices/helium/helium-contents",
-              "wxm-devices/helium/helium-assemble",
-  						"wxm-devices/helium/helium-install-weather-station",
-              "wxm-devices/helium/helium-connect-and-claim",
-              "wxm-devices/helium/helium-adding-wallets",
-              "wxm-devices/helium/helium-troubleshooting",
-  					],
-  					collapsed: false,
-				},
+        }
       ],
       collapsed: false,
     },
