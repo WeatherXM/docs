73bc73d555c829200d63918a8bcd25a65c764c32
tasos <lazarou.ts@gmail.com>
2023-01-19 16:23:16 +0200

    iOS caps

--- src/components/iOS.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Highlight from "./Highlight";
-export default function Ios() {
-  return (
-    <span>
-      <Highlight color="#da5552">iOS</Highlight>
-    </span>
-  );
-}
