f91471e61361bfa6fbd9136bff03cd1cada59c53
tasos <lazarou.ts@gmail.com>
2023-01-19 16:23:26 +0200

    Rename file

---
+++ src/components/IOS.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Highlight from "./Highlight";
+export default function IOS() {
+  return (
+    <span>
+      <Highlight color="#da5552">iOS</Highlight>
+    </span>
+  );
+}
