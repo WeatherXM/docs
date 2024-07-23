67ad23fbd6c2a9041de67f8ee14b3d3a5762dbb8
Pavlos Tzegiannakis <pavlostzeg@gmail.com>
2024-07-23 23:36:34 +0300

    Update sidebar labels to all other products for consistency

--- sidebars.js
+++ sidebars.js
@@ -8,21 +8,21 @@ module.exports = {
       items: [
 	    {
 	      type: "category",
-	      label: "WeatherXM M5 bundle (WB1000)",
+	      label: "M5 (WB1000)",
 	      items: [
 					"wxm-devices/wifi-m5-bundle/wxm-ws1000-introduction",
 					"wxm-devices/wifi-m5-bundle/m5-contents",
 					"wxm-devices/wifi-m5-bundle/m5-assemble",
 					"wxm-devices/wifi-m5-bundle/m5-install-weather-station",
 					"wxm-devices/wifi-m5-bundle/m5-connect-and-claim",
-					"wxm-devices/wifi-m5-bundle/m5-troubleshooting",
-					"wxm-devices/wifi-m5-bundle/m5-maintenance"
+					"wxm-devices/wifi-m5-bundle/m5-maintenance",
+					"wxm-devices/wifi-m5-bundle/m5-troubleshooting"
 	      ],
 	      collapsed: true,
 	    },
 	    {
 	      type: "category",
-	      label: "WeatherXM Helium station (WS2000)" ,
+	      label: "Helium (WS2000)" ,
 	      items: [
 					"wxm-devices/helium-bundle/wxm-ws2000-introduction",
 					"wxm-devices/helium-bundle/helium-contents",
@@ -30,29 +30,29 @@ module.exports = {
 					"wxm-devices/helium-bundle/helium-connect-and-claim",
 					"wxm-devices/helium-bundle/helium-update-firmware",
 					"wxm-devices/helium-bundle/helium-install-weather-station",
-					"wxm-devices/helium-bundle/helium-troubleshooting",
-					"wxm-devices/helium-bundle/helium-maintenance"
+					"wxm-devices/helium-bundle/helium-maintenance",
+					"wxm-devices/helium-bundle/helium-troubleshooting"
 	      ],
 	      collapsed: true,
 		  
 	    },
 		{
 			type: "category",
-			label: "WeatherXM D1 bundle (WB1200)",
+			label: "D1 (WB1200)",
 			items: [
 					  "wxm-devices/D1-bundle/D1-bundle-introduction",
 					  "wxm-devices/D1-bundle/D1-contents",
 					  "wxm-devices/D1-bundle/D1-assemble",
 					  "wxm-devices/D1-bundle/install-weather-station",
 					  "wxm-devices/D1-bundle/D1-connect-and-claim",
-					  "wxm-devices/D1-bundle/D1-troubleshooting",
-					  "wxm-devices/D1-bundle/maintenance"
+					  "wxm-devices/D1-bundle/maintenance",
+					  "wxm-devices/D1-bundle/D1-troubleshooting"
 			],
 			collapsed: true,
 		  },
 		  {
 			type: "category",
-			label: "WeatherXM Pulse bundle (WB3000)",
+			label: "Pulse (WB3000)",
 			items: [
 					  "wxm-devices/pulse-bundle/introduction",
 					  "wxm-devices/pulse-bundle/contents",
