47970b28f03cbe144b4e5b7a4bb7e74bca3967c6
Pavlos Tzegiannakis <pavlostzeg@gmail.com>
2024-07-30 16:03:12 +0300

    FE-1044 Feature/url fixes (#132)
    * Fix ' vs ", fix indentation and fix mobile-app related URLs.
    
    * Fix Rewards-related URLs
    
    * Update Glossary and FAQ
    
    * Fix D1 and deployment URLs.
    
    * Remove unnecessary parameters in document init (e.g. slug, id, hide_title=false etc)
    
    * Add redirection URLs for D1
    
    * Update Pulse URLs
    
    * Update Helium URLs
    
    * Update M5 URLs
    
    * Move all redirects to an explicit file redirects.js
    
    * Fix URL in home

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,133 +3,112 @@ require("dotenv").config();
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: "WeatherXM Docs",
-    tagline: "WeatherXM Documentation Page",
-    url: "https://docs.weatherxm.com",
-    baseUrl: "/",
-    onBrokenLinks: "throw",
-    onBrokenMarkdownLinks: "warn",
-    favicon: "img/favicon/favicon.ico",
-    organizationName: "WeatherXM", // Usually your GitHub org/user name.
-    projectName: "docs", // Usually your repo name.
-    deploymentBranch: "gh-pages",
-    trailingSlash: false,
-
-    plugins: [
-        path.resolve(__dirname, "plugins/docusaurus-plugin-hotjar"),
-        [
-            "@docusaurus/plugin-client-redirects",
-            {
-                redirects: [
-                    {
-                        to: "/rewards/how-to-join-arbitrum-and-see-your-tokens",
-                        from: "/wallet/how-to-join-arbitrum-and-see-your-tokens",
-                    },
-                    {
-                        to: "/wxm-devices/wifi-m5-bundle/wxm-ws1000-introduction#features-and-specifications",
-                        from: "/m5-specs",
-                    },
-                    {
-                        to: "/wxm-devices/helium-bundle/wxm-ws2000-introduction#features-and-specifications",
-                        from: "/helium-specs",
-                    },
-                    {
-                        to: "/wxm-devices/D1-bundle/D1-bundle-introduction#features-and-specifications",
-                        from: "/wxm-devices/D1-bundle/D1-specs",
-                    }
-                ]
-            }
-        ]
+  title: "WeatherXM Docs",
+  tagline: "WeatherXM Documentation Page",
+  url: "https://docs.weatherxm.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon/favicon.ico",
+  organizationName: "WeatherXM", // Usually your GitHub org/user name.
+  projectName: "docs", // Usually your repo name.
+  deploymentBranch: "gh-pages",
+  trailingSlash: false,
+  plugins: [
+    path.resolve(__dirname, "plugins/docusaurus-plugin-hotjar"),
+    [
+      "@docusaurus/plugin-client-redirects",
+      {
+        redirects: require("./redirects.js")
+      }
+    ]
+  ],
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        gtag: {
+          trackingID: "G-E38CY3JRM3"
+        },
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: "/",
+          // Please change this to your repo.
+          editUrl: "https://github.com/WeatherXM/docs/blob/main",
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex]
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css")
+        },
+      },
     ],
-
-    presets: [
-        [
-            "@docusaurus/preset-classic",
-            {
-                gtag: {
-                    trackingID: "G-E38CY3JRM3",
-                },
-                docs: {
-                    sidebarPath: require.resolve("./sidebars.js"),
-                    routeBasePath: "/",
-                    // Please change this to your repo.
-                    editUrl: "https://github.com/WeatherXM/docs/blob/main",
-                    remarkPlugins: [remarkMath],
-                    rehypePlugins: [rehypeKatex],
-                },
-                theme: {
-                    customCss: require.resolve("./src/css/custom.css"),
-                },
-            },
+  ],
+  themeConfig: {
+    hotjar: {
+      applicationId: 2542811
+    },
+    colorMode: {
+      defaultMode: "light",
+      disableSwitch: false,
+      respectPrefersColorScheme: true
+    },
+    navbar: {
+      logo: {
+        alt: "WeatherXM",
+        src: "img/wxm-logo-wide-notagline.svg",
+        srcDark: "img/wxm-logo-wide-notagline-white.svg"
+      },
+      items: [
+          {
+            href: "https://explorer.weatherxm.com/",
+            label: "Explorer",
+            position: "right"
+          },
+          {
+            href: "https://claim.weatherxm.com/",
+            label: "Claim Rewards",
+            position: "right"
+          },
+          {
+            href: "https://weatherxm.com/whitepaper",
+            label: "Whitepaper",
+            position: "right"
+          },
+          {
+            href: "https://github.com/WeatherXM/docs",
+            label: "GitHub",
+            position: "right"
+          },
+          {
+            href: "https://support.weatherxm.com",
+            label: "Help Center",
+            position: "right"
+          },
+          {
+            href: "https://shop.weatherxm.com",
+            label: "Shop",
+            position: "right",
+            id: "docs"
+          },
         ],
-    ],
-
-    themeConfig: {
-        hotjar: {
-            applicationId: 2542811,
-        },
-        colorMode: {
-            defaultMode: "light",
-            disableSwitch: false,
-            respectPrefersColorScheme: true,
-        },
-        navbar: {
-            logo: {
-                alt: "WeatherXM",
-                src: "img/wxm-logo-wide-notagline.svg",
-                srcDark: "img/wxm-logo-wide-notagline-white.svg",
-            },
-            items: [
-                {
-                    href: "https://explorer.weatherxm.com/",
-                    label: "Explorer",
-                    position: "right",
-                },
-                {
-                    href: "https://claim.weatherxm.com/",
-                    label: "Claim Rewards",
-                    position: "right",
-                },
-                {
-                    href: "https://weatherxm.com/whitepaper",
-                    label: "Whitepaper",
-                    position: "right",
-                },
-                {
-                    href: "https://github.com/WeatherXM/docs",
-                    label: "GitHub",
-                    position: "right",
-                },
-                {
-                    href: "https://support.weatherxm.com",
-                    label: "Help Center",
-                    position: "right",
-                },
-                {
-                    href: "https://shop.weatherxm.com",
-                    label: "Shop",
-                    position: "right",
-                    id: "docs",
-                },
-            ],
-        },
-        footer: {
-            copyright: `Copyright © ${new Date().getFullYear()} WeatherXM`,
-            style: "light",
-        },
-    },
-
-stylesheets: [
+      },
+      footer: {
+        copyright: `Copyright © ${new Date().getFullYear()} WeatherXM`,
+        style: "light"
+      },
+  },
+  stylesheets: [
     {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
-      integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
-    },
-  ],   
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
+      integrity: "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous"
+    }
+  ]
 };
 
 module.exports = config;
--- sidebars.js
+++ sidebars.js
@@ -1,109 +1,107 @@
 module.exports = {
   docs: [
     "home",
-	"introduction",
+    "introduction",
     {
       type: "category",
       label: "Deploy a WeatherXM Device",
       items: [
-	    {
-	      type: "category",
-	      label: "M5 (WB1000)",
-	      items: [
-					"wxm-devices/wifi-m5-bundle/wxm-ws1000-introduction",
-					"wxm-devices/wifi-m5-bundle/m5-contents",
-					"wxm-devices/wifi-m5-bundle/m5-assemble",
-					"wxm-devices/wifi-m5-bundle/m5-install-weather-station",
-					"wxm-devices/wifi-m5-bundle/m5-connect-and-claim",
-					"wxm-devices/wifi-m5-bundle/m5-maintenance",
-					"wxm-devices/wifi-m5-bundle/m5-troubleshooting"
-	      ],
-	      collapsed: true,
-	    },
-	    {
-	      type: "category",
-	      label: "Helium (WS2000)" ,
-	      items: [
-					"wxm-devices/helium-bundle/wxm-ws2000-introduction",
-					"wxm-devices/helium-bundle/helium-contents",
-					"wxm-devices/helium-bundle/helium-assemble",
-					"wxm-devices/helium-bundle/helium-connect-and-claim",
-					"wxm-devices/helium-bundle/helium-update-firmware",
-					"wxm-devices/helium-bundle/helium-install-weather-station",
-					"wxm-devices/helium-bundle/helium-maintenance",
-					"wxm-devices/helium-bundle/helium-troubleshooting"
-	      ],
-	      collapsed: true,
-		  
-	    },
-		{
-			type: "category",
-			label: "D1 (WB1200)",
-			items: [
-					  "wxm-devices/D1-bundle/D1-bundle-introduction",
-					  "wxm-devices/D1-bundle/D1-contents",
-					  "wxm-devices/D1-bundle/D1-assemble",
-					  "wxm-devices/D1-bundle/install-weather-station",
-					  "wxm-devices/D1-bundle/D1-connect-and-claim",
-					  "wxm-devices/D1-bundle/maintenance",
-					  "wxm-devices/D1-bundle/D1-troubleshooting"
-			],
-			collapsed: true,
-		  },
-		  {
-			type: "category",
-			label: "Pulse (WB3000)",
-			items: [
-					  "wxm-devices/pulse-bundle/introduction",
-					  "wxm-devices/pulse-bundle/contents",
-					  "wxm-devices/pulse-bundle/assemble-weather-station",
-					  "wxm-devices/pulse-bundle/install-weather-station",
-					  "wxm-devices/pulse-bundle/connect-and-claim",
-					  "wxm-devices/pulse-bundle/maintenance",
-					  "wxm-devices/pulse-bundle/troubleshooting"
-			],
-			collapsed: true,
-		  },
-		"wxm-devices/deployment-examples",
-      ],
-      collapsed: true,  
+        {
+          type: "category",
+          label: "M5 (WB1000)",
+          items: [
+            "wxm-devices/m5/introduction",
+            "wxm-devices/m5/contents",
+            "wxm-devices/m5/assemble",
+            "wxm-devices/m5/install-weather-station",
+            "wxm-devices/m5/connect-and-claim",
+            "wxm-devices/m5/maintenance",
+            "wxm-devices/m5/troubleshooting"
+          ],
+          collapsed: true
+        },
+        {
+          type: "category",
+          label: "Helium (WS2000)" ,
+          items: [
+            "wxm-devices/helium/introduction",
+            "wxm-devices/helium/contents",
+            "wxm-devices/helium/assemble",
+            "wxm-devices/helium/connect-and-claim",
+            "wxm-devices/helium/update-firmware",
+            "wxm-devices/helium/install-weather-station",
+            "wxm-devices/helium/maintenance",
+            "wxm-devices/helium/troubleshooting"
+          ],
+          collapsed: true
+        },
+        {
+          type: "category",
+          label: "D1 (WB1200)",
+          items: [
+            "wxm-devices/d1/introduction",
+            "wxm-devices/d1/contents",
+            "wxm-devices/d1/assemble",
+            "wxm-devices/d1/install-weather-station",
+            "wxm-devices/d1/connect-and-claim",
+            "wxm-devices/d1/maintenance",
+            "wxm-devices/d1/troubleshooting"
+          ],
+          collapsed: true
+          },
+          {
+            type: "category",
+            label: "Pulse (WB3000)",
+            items: [
+              "wxm-devices/pulse/introduction",
+              "wxm-devices/pulse/contents",
+              "wxm-devices/pulse/assemble",
+              "wxm-devices/pulse/install-weather-station",
+              "wxm-devices/pulse/connect-and-claim",
+              "wxm-devices/pulse/maintenance",
+              "wxm-devices/pulse/troubleshooting"
+            ],
+            collapsed: true
+          },
+          "wxm-devices/deployment-examples",
+        ],
+        collapsed: true,
     },
-	{
-		type: "category",
-		label: "Rewards",
-		items: [
-				  "rewards/reward-mechanism",
-				  "rewards/quality-of-data",
-				  "rewards/proof-of-location",
-				  "rewards/cell-capacity",
-				  "rewards/hardware-class",
-				  "rewards/reward-boosts",
-				  "rewards/rewards-troubleshooting",
-				  "rewards/rewards-claiming",
-				  "rewards/how-to-join-arbitrum-and-see-your-tokens"
-		],
-		collapsed: true,
-	  },
+    {
+      type: "category",
+      label: "Rewards",
+      items: [
+        "rewards/reward-mechanism",
+        "rewards/quality-of-data",
+        "rewards/proof-of-location",
+        "rewards/cell-capacity",
+        "rewards/hardware-class",
+        "rewards/reward-boosts",
+        "rewards/rewards-troubleshooting",
+        "rewards/rewards-claiming",
+        "rewards/how-to-join-arbitrum-and-see-your-tokens"
+      ],
+      collapsed: true,
+    },
 	  {
-		type: "category",
-		label: "Mobile App",
-
-		items: [
-			{
-				type: "category",
-				label: "Wallet",
-				items: [
-					"wallet/add-edit-wallet-address"
-				],
-				collapsed: true,
-			},
-            "account/delete-account",
-            "account/change-password"
-		],
-		collapsed: true,
+      type: "category",
+      label: "Mobile App",
+      items: [
+        {
+          type: "category",
+          label: "Wallet",
+          items: [
+            "mobile-app/wallet/add-edit-wallet-address"
+          ],
+          collapsed: true,
+        },
+        "mobile-app/delete-account",
+        "mobile-app/change-password"
+      ],
+      collapsed: true,
 	  },
-	"tokenomics",
-	"glossary/glossary",
-	"help-center/faqs"
+    "tokenomics",
+    "glossary",
+    "faq"
   ]
 };
